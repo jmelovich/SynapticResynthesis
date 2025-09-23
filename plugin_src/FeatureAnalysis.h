1c0a2f9053f41d4ce40ed2edcb39eae6c1341ab3
PatrickKallenbach <pkallenbach1@ufl.edu>
2025-09-23 18:21:03 -0400

    Harmonicity, and conslidated ai and aifi sums

--- plugin_src/FeatureAnalysis.h
+++ plugin_src/FeatureAnalysis.h
@@ -5,33 +5,18 @@
 
 class FeatureAnalysis
 {
+public:
   static std::pair<float, float> FundamentalFrequency(float* input, int inputSize)
   {
     return {-1.0, -1.0}; // TODO
   }
 
   static float Affinity(float* input, int inputSize, int sampleRate, std::pair<float, float> fund) {
-    float frequencyStep = (float)sampleRate / inputSize;
-
-    float aifi = input[1] * (sampleRate / 2);
-    for (int i = 1; i < inputSize; i++)
-    {
-      float mag = std::sqrt(input[2 * i] * input[2 * i] + input[2 * i + 1] * input[2 * i + 1]);
-      aifi += mag * frequencyStep * i;
-    }
-
-    float ai = 0;
-    for (int i = 1; i < inputSize; i++)
-    {
-      float mag = std::sqrt(input[2 * i] * input[2 * i] + input[2 * i + 1] * input[2 * i + 1]);
-      ai += mag;
-    }
-
-    return aifi / (fund.first * ai);
+    return sum_aifi(input, inputSize, sampleRate) / (fund.first * sum_ai(input, inputSize));
   }
 
   static float Sharpness(float* input, int inputSize, int sampleRate, std::pair<float, float> fund) {
-    return -1.0; // TODO
+    return fund.second / sum_ai(input, inputSize);
   }
 
   static float Harmonicity(float* input, int inputSize, int sampleRate, std::pair<float, float> fund) {
@@ -48,5 +33,32 @@ class FeatureAnalysis
 
   static float MeanContrast(float* input, int inputSize, int sampleRate, std::pair<float, float> fund) {
     return -1.0; // TODO
+  }
+
+  private:
+  static float sum_ai(float* input, int inputSize)
+  {
+    float ai = input[1]; // nyquist (per pffft implementation)
+    for (int i = 1; i < inputSize; i++)
+    {
+      float mag = std::sqrt(input[2 * i] * input[2 * i] + input[2 * i + 1] * input[2 * i + 1]);
+      ai += mag;
+    }
+
+    return ai;
+  }
+
+  static float sum_aifi(float* input, int inputSize, int sampleRate)
+  {
+    float frequencyStep = (float)sampleRate / inputSize;
+
+    float aifi = input[1] * (sampleRate / 2); // nyquist (per pffft implementation)
+    for (int i = 1; i < inputSize; i++)
+    {
+      float mag = std::sqrt(input[2 * i] * input[2 * i] + input[2 * i + 1] * input[2 * i + 1]);
+      aifi += mag * frequencyStep * i;
+    }
+
+    return aifi;
   }
 };
